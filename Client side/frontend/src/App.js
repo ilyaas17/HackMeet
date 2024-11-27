c6b00350eb9a61c80ce0ed61bde21a8264aa6638
Harendra Prajapat <149884737+Pinti0001@users.noreply.github.com>
2024-11-27 17:36:48 +0530

    Revert "Testing"

---
+++ Client side/frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+
+      <Navbar/>
+      <Home/>
+      <Footer/>
+
+    </>
+  );
+}
+
+export default App;
