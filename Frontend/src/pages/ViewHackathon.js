a26c05db0923b3bde74596bc45908d59f11e9102
Prateek-Fst <147906999+Prateek-Fst@users.noreply.github.com>
2024-11-28 16:33:12 +0530

    added

--- Frontend/src/pages/ViewHackathon.js
+++ Frontend/src/pages/ViewHackathon.js
@@ -27,7 +27,7 @@ const ViewHackathon = () => {
     }
 
     return (
-        <div className="bg-gray-100 py-8">
+        <div className="bg-gray-100 py-8 mt-20">
             <div className="container mx-auto px-4">
                 <h1 className="text-4xl font-bold text-center mb-6">All Hackathons</h1>
 
